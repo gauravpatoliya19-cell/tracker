1931d699d4e9a870337c0d9ea0d7e65b96a3a692
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-19 18:49:30 +0530

    Refactor application configuration in app.php
    Removed application configuration and kept middleware setup.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,4 @@
-<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        // આ લાઇન પ્રોક્સી સર્વર (Render) ને ટ્રસ્ટ કરીને સાચો IP આપશે
-        $middleware->trustProxies(at: '*'); 
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+->withMiddleware(function (Middleware $middleware) {
+    // આ લાઇન Render ના સાચા IP ને પકડવા માટે ફરજિયાત છે
+    $middleware->trustProxies(at: '*'); 
+})
