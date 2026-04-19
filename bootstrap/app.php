e6c79967417ab683c0993c7fd2940e06ed586d8f
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-19 18:02:23 +0530

    Update middleware to trust proxies with settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies; // આ ઉમેરવાની જરૂર પડી શકે છે
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,10 +11,17 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-->withMiddleware(function (Middleware $middleware) {
-    // આ લાઇન Render ના Load Balancer ને ઓળખવામાં મદદ કરશે
-    $middleware->trustProxies(at: '*'); 
-})
+    ->withMiddleware(function (Middleware $middleware) {
+        // ૧. Render અને Cloudflare જેવા પ્રોક્સી સર્વર માટે ટ્રસ્ટ સેટિંગ
+        // આ લાઇન ખાતરી કરશે કે $request->ip() હંમેશા સાચો યુઝર IP આપે.
+        $middleware->trustProxies(at: '*');
+
+        // ૨. વૈકલ્પિક: જો હજી પણ પ્રોબ્લેમ આવે, તો સ્પેસિફિક હેડર્સ સેટ કરી શકાય
+        $middleware->trustProxies(headers: TrustProxies::HEADER_X_FORWARDED_FOR | 
+                                           TrustProxies::HEADER_X_FORWARDED_HOST | 
+                                           TrustProxies::HEADER_X_FORWARDED_PORT | 
+                                           TrustProxies::HEADER_X_FORWARDED_PROTO);
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
