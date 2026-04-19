6a8888bb1edd0b556f2f869e808b43902d4a1c37
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-19 18:07:25 +0530

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies; // આ ઉમેરવાની જરૂર પડી શકે છે
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,15 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // ૧. Render અને Cloudflare જેવા પ્રોક્સી સર્વર માટે ટ્રસ્ટ સેટિંગ
-        // આ લાઇન ખાતરી કરશે કે $request->ip() હંમેશા સાચો યુઝર IP આપે.
+        // Render માટે સાચો IP મેળવવા માટે આટલું જ પૂરતું છે
         $middleware->trustProxies(at: '*');
-
-        // ૨. વૈકલ્પિક: જો હજી પણ પ્રોબ્લેમ આવે, તો સ્પેસિફિક હેડર્સ સેટ કરી શકાય
-        $middleware->trustProxies(headers: TrustProxies::HEADER_X_FORWARDED_FOR | 
-                                           TrustProxies::HEADER_X_FORWARDED_HOST | 
-                                           TrustProxies::HEADER_X_FORWARDED_PORT | 
-                                           TrustProxies::HEADER_X_FORWARDED_PROTO);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
