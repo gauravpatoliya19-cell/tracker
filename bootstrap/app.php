83386387dd88e0852addc97bb988842164c1f679
Gaurav Patoliya <gauravpatoliya19@gmail.com>
2026-04-20 17:30:59 +0530

    Refactor middleware setup in app.php
    Updated middleware configuration and added comments in Gujarati.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,19 @@
-->withMiddleware(function (Middleware $middleware) {
-    // આ લાઇન Render ના સાચા IP ને પકડવા માટે ફરજિયાત છે
-    $middleware->trustProxies(at: '*'); 
-})
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // સ્પેલિંગ ચેક કરજો: trustProxies (છેલ્લે 'ies' આવશે, 'ist' નહીં)
+        $middleware->trustProxies(at: '*'); 
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
